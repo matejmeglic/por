f61e3e4e8ea83fe7fc8015e6f169fcd3ce682241
matejmeglic <matej@matejmeglic.com>
2020-05-08 22:03:22 +0200

    Boris is alive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 class App extends Component {
   state = {
-    contents: null,
+    contents: [],
   };
 
   componentDidMount() {
@@ -12,23 +12,34 @@ class App extends Component {
  
   callApi = () => {
     fetch("https://api.github.com/repos/matejmeglic/por/contents/public/img/")
-    .then((res) => res.json)
+    .then((res) => res.json())
     .then((res) => {
-      console.log(res);
-  
-    } 
-    )
-    
-  
-  
+      let counter = res.length-1;
+      let list = [];
+       while (counter >= 0) {
+       list.push(res[counter].name);
+       counter--;}
+       this.setState({
+        contents: list,
+      });
+      } 
+    )  
   }
  
   render() {
- 
+  const {contents} = this.state;
+  console.log(contents);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="intro">
+        <h1>Porkola-Pipola-Boris</h1>
+        <p>Opazuj rast pora Borisa</p>
+      <div className="App-header">
+        {
+          contents.map(element => <div className="container"><img className="photo" src={`/img/${element}`} alt={`${element}`} key={`${element}`} /></div>)
+        }
+      </div>
       </header>
     </div>
   );
